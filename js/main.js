f4d9e8711c14cc14801c143faace07de36540e81
Serhiy Hulyi <serhiyhulyi93@gmail.com>
2015-12-16 11:26:35 +0200

    tamplete

---
+++ js/main.js
@@ -0,0 +1,37 @@
+/*
+#############################
+#   Main JS for ____________   #
+#############################
+*/
+
+jQuery(document).ready(function($) {
+
+
+/*-----------------------------------------------------------------*/  
+  $('.magnific').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+    modal: true,
+
+    closeBtnInside: true,
+    preloader: false,
+    
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
+
+  $(document).on('click', '.popup-modal-dismiss', function (e) {
+    e.preventDefault();
+    $.magnificPopup.close();
+  });
+
+});
+
+
+
+
